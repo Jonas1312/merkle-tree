1feb361316fe45d55727774004b4c37770320e9b
Julien <1995juju@laposte.net>
2017-04-25 23:18:50 +0200

    modif 25/04/17 23:18

--- src/test_merkle_tree.py
+++ src/test_merkle_tree.py
@@ -14,30 +14,30 @@ class merkle_tree_test(unittest.TestCase):
     """Tests the merkle_tree functions"""
 
     def setUp(self):
-        """Initialisation"""
-        MerkleTree1 = MerkleTree.__init__(self,8)
+        """Initialization"""
+        self.merkle_tree1 = MerkleTree(8)
 
     def test_add_node(self):
         """Tests add_node"""
 
         data1 = None
-        data2 = hex(25)
-        data3 = bytearray.fromhex(data2())
+        data2 = 'A6'
+        data3 = bytearray.fromhex('A6')
         data4 = "test"
 
-        position1 = [0,0]
-        position2 = [1,2]
-        position3 = [2,1]
-        position4 = [0,3]
+        position1 = 0,0
+        position2 = 1,2
+        position3 = 2,1
+        position4 = 0,3
 
-        add_node(merkle_tree_test,data1,position1)
-        add_node(merkle_tree_test, data2, position2,True)
-        add_node(merkle_tree_test, data3, position3,True)
-        add_node(merkle_tree_test, data4, position4)
+        self.merkle_tree1.add_node(data1, position1)
+        self.merkle_tree1.add_node(data2, position2, True)
+        self.merkle_tree1.add_node(data3, position3, True)
+        self.merkle_tree1.add_node(data4, position4)
 
-        assertIsNone(merkle_tree_test[position1])
-        assertIs(bytearray.fromhex(data2),merkle_tree_test[position2])
-        assertIs(data3,merkle_tree_test[position3])
-        assertIs(bytearray(hashlib.sha256(data4.encode('utf-8')).digest()),merkle_tree_test[position4])
+        self.assertIsNone(self.merkle_tree1.tree[position1])
+        self.assertIs(bytearray.fromhex(data2),self.merkle_tree1.tree[position2])
+        self.assertIs(data3,self.merkle_tree1.tree[position3])
+        self.assertIs(bytearray(hashlib.sha256(data4.encode('utf-8')).digest()),self.merkle_tree1.tree[position4])
 
 
