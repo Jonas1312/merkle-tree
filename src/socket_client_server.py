637374318966526f59920a19146095554ba05cdb
Jonas <Jonas1312@users.noreply.github.com>
2017-04-27 12:25:56 +0200

    Add socket client/server classes

---
+++ src/socket_client_server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+  Purpose:  Socket
+  Created:  27/04/2017
+"""
+
+import socket
+import threading
+
+
+class Client:
+    """Client class."""
+
+    def __init__(self, host="localhost", port=12800):
+        """Client object constructor."""
+        self.ip = host
+        self.port = port
+        self.sock = socket.socket()
+        self.sock.connect((host, port))
+        print("Client connected to {}:{}".format(host, port))
+
+    def send(self, data):
+        if type(data) is str:
+            self.sock.sendall(data.encode())
+        else:
+            self.sock.sendall(data)
+
+    def close(self):
+        self.sock.close()
+        print("Client closed")
+
+
+class Server:
+    """Server class."""
+
+    def __init__(self, port=12800):
+        """Server object constructor."""
+        self.port = port
+        self.sock = socket.socket()
+        self.sock.bind(('', port))
+        self.sock.listen(5)
+        self.client, self.address = self.sock.accept()
+        print("Server connected to {}".format(self.address))
+
+    def receive(self):
+        return self.client.recv(2 ** 15)
+
+    def close(self):
+        self.client.close()
+        self.sock.close()
+        print("Server closed")
